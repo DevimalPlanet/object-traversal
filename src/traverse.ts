d031f17f69871e4c2911bd33ca272898651aed30
Devimal <devimalplanet@gmail.com>
2022-10-23 13:21:03 +0300

    fix: remove redundant line

--- src/traverse.ts
+++ src/traverse.ts
@@ -101,7 +101,6 @@ const _traverse = (
         continue;
       }
 
-      newNodesToVisit.reset();
       const keys = Object.keys(value);
       for (let i = 0; i < keys.length; i++) {
         const property = keys[i];
